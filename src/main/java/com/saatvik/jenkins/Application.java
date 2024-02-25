3d0ff1f448e4f503428c32148d228a59ebf47c79
Birudeo Garande <birudeogarande@gmail.com>
2024-02-25 17:58:34 +0530

    added mockmvc test

--- src/main/java/com/saatvik/jenkins/Application.java
+++ src/main/java/com/saatvik/jenkins/Application.java
@@ -2,12 +2,21 @@ package com.saatvik.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/hello/{name}")
+	public String greeting(@PathVariable String name) {
+		return "Hello " + name + " welcome to devops role!";
 	}
 
 }
--- src/test/java/com/saatvik/jenkins/ApplicationTests.java
+++ src/test/java/com/saatvik/jenkins/ApplicationTests.java
@@ -1,13 +1,43 @@
 package com.saatvik.jenkins;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Autowired
+	WebApplicationContext context;
+
+	MockMvc mockMvc;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+
+	
+	@Test
+	public void greetingTestPass() throws Exception {
+		String name = "Birudeo Garande";		
+		mockMvc.perform(get("/hello/{name}", name))
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("Hello " + name + " welcome to devops role!")));
+	}
 }
